e49ac8392842d6eae5de070c794ca49b0ebe7265
shoile abdulazeez <shoabdulazeez@gmail.com>
2025-07-17 14:42:57 +0000

    update

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import WelcomeMessage from './components/WelcomeMessage.jsx';
 import Footer from './components/Footer.jsx';
-import MainContent   from './components/MainContent.jsx';
+import MainContent    from './components/MainContent.jsx';
 import Header from './components/Header.jsx';
 import './App.css'
 
