a0e5419a1fa47ce5a9934b5a53816e49b0fb8b80
shoile abdulazeez <shoabdulazeez@gmail.com>
2025-07-22 11:17:43 +0000

    duplicate last weeek reract app file

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import WelcomeMessage from './components/WelcomeMessage.jsx';
+import Footer from './Footer.jsx';
+import MainContent from './MainContent.jsx';
+import Header from './Header.jsx';
+import UserProfile from './components/UserProfile.jsx';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <h2>My Favorite Cities</h2>
+      <p>Learn React</p>
+    </>
+  )
+}
+
+export default App
