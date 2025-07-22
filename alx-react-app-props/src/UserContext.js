28ad483c895ac3370d768137dd395c87247c6965
shoile azeez <shoabdulazeez@gmail.com>
2025-07-22 23:24:31 +0100

    create a props add

---
+++ alx-react-app-props/src/UserContext.js
@@ -0,0 +1,6 @@
+import { createContext } from 'react';
+
+// Create UserContext
+const UserContext = createContext();
+
+export default UserContext;
---
+++ alx-react-app-props/src/components/WelcomeMessage.jsx
@@ -0,0 +1,10 @@
+function WelcomeMessage() {
+    return (
+        <div>
+            <h1 style={{ color: "blue", fontSize: "24px"}}>Hello everyone, I am learning React at ALX!</h1>
+            <p>I am learning about JSX!</p>
+        </div>
+    );
+}
+
+export default WelcomeMessage;
