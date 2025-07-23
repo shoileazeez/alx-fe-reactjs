57dcc382377f1240caf601a8343c49b5ef264af6
shoile azeez <shoabdulazeez@gmail.com>
2025-07-23 16:15:17 +0100

    move the recipestore cod to componets

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
-import useRecipeStore from './recipeStore';
+import useRecipeStore from './components/recipeStore';
 import './App.css';
 
 function App() {
