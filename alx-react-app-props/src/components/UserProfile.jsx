00facf7a345a8ebcacc5e6ea395fdded89458eab
shoile azeez <shoabdulazeez@gmail.com>
2025-07-25 02:41:53 +0100

    add the context in my user profile page

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,10 +1,15 @@
-const UserProfile = (props) => {
-   return (
-     <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-      <h2 style={{ color: 'blue' }}>{props.name}</h2>
-      <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
-      <p>Bio: {props.bio}</p>
-      </div>
-   );
+import { useContext } from 'react';
+import UserContext from '../UserContext';
+
+const UserProfile = () => {
+  const userData = useContext(UserContext);
+
+  return (
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+      <h2 style={{ color: 'blue' }}>{userData.name}</h2>
+      <p>Age: <span style={{ fontWeight: 'bold' }}>{userData.age}</span></p>
+      <p>Bio: {userData.bio}</p>
+    </div>
+  );
 };
 export default UserProfile;
