9abda986776ad6e7d1c549fdb0fba71405f2a23a
shoile azeez <shoabdulazeez@gmail.com>
2025-08-08 00:28:57 +0100

    add some responsive designing

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-36 h-36 mx-auto"/>
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+    <div className="bg-gray-100 p-8 sm:p-8 md:p-6 max-w-sm sm:max-w-sm md:max-w-md mx-auto my-20 rounded-lg shadow-lg">
+      <img src="https://gratisography.com/photo/augmented-reality/" alt="User" className="rounded-full sm:w-36 sm:h-36 md:w-36 md:h-36 mx-auto"/>
+      <h1 className="text-xl sm:text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
+      <p className="text-gray-600 sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
 }
